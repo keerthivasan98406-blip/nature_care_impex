fd1f3ab5d0dfda35e39e787ee6baf6eb63c325e9
keerthivasan98406-blip <keerthivasan98406@gmail.com>
2026-01-16 16:34:16 +0530

    Fix MIME type issues for Render deployment - serve files from root with proper content types

--- server/server.js
+++ server/server.js
@@ -14,77 +14,107 @@ connectDB();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, '..')));
+
+// Set proper MIME types for static files
+app.use((req, res, next) => {
+  if (req.path.endsWith('.js')) {
+    res.type('application/javascript');
+  } else if (req.path.endsWith('.css')) {
+    res.type('text/css');
+  } else if (req.path.endsWith('.html')) {
+    res.type('text/html');
+  }
+  next();
+});
+
+// Serve static files with proper MIME types
+app.use(express.static(path.join(__dirname, '..'), {
+  setHeaders: (res, filePath) => {
+    if (filePath.endsWith('.js')) {
+      res.setHeader('Content-Type', 'application/javascript');
+    } else if (filePath.endsWith('.css')) {
+      res.setHeader('Content-Type', 'text/css');
+    }
+  }
+}));
 
 // Serve static files from html, css, js, and assets folders
-app.use(express.static(path.join(__dirname, '..', 'html')));
-app.use(express.static(path.join(__dirname, '..', 'css')));
-app.use(express.static(path.join(__dirname, '..', 'js')));
-app.use(express.static(path.join(__dirname, '..', 'assets')));
+app.use('/html', express.static(path.join(__dirname, '..', 'html')));
+app.use('/css', express.static(path.join(__dirname, '..', 'css')));
+app.use('/js', express.static(path.join(__dirname, '..', 'js')));
+app.use('/assets', express.static(path.join(__dirname, '..', 'assets')));
 
-// Routes for HTML files
+// Routes for HTML files - serve from root directory
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 app.get('/index.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
 
 app.get('/owner', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'owner.html'));
+  res.sendFile(path.join(__dirname, '..', 'owner.html'));
 });
 
 app.get('/owner.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'owner.html'));
+  res.sendFile(path.join(__dirname, '..', 'owner.html'));
 });
 
 app.get('/products', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'products.html'));
+  res.sendFile(path.join(__dirname, '..', 'products.html'));
 });
 
 app.get('/products.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'products.html'));
+  res.sendFile(path.join(__dirname, '..', 'products.html'));
 });
 
 app.get('/contact', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'contact.html'));
+  res.sendFile(path.join(__dirname, '..', 'contact.html'));
 });
 
 app.get('/contact.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'contact.html'));
+  res.sendFile(path.join(__dirname, '..', 'contact.html'));
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'about.html'));
+  res.sendFile(path.join(__dirname, '..', 'about.html'));
 });
 
 app.get('/about.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'about.html'));
+  res.sendFile(path.join(__dirname, '..', 'about.html'));
 });
 
 app.get('/payment', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'payment.html'));
+  res.sendFile(path.join(__dirname, '..', 'payment.html'));
 });
 
 app.get('/payment.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'payment.html'));
+  res.sendFile(path.join(__dirname, '..', 'payment.html'));
 });
 
 app.get('/order-details', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'order-details.html'));
+  res.sendFile(path.join(__dirname, '..', 'order-details.html'));
 });
 
 app.get('/order-details.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'order-details.html'));
+  res.sendFile(path.join(__dirname, '..', 'order-details.html'));
 });
 
 app.get('/product-detail', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'product-detail.html'));
+  res.sendFile(path.join(__dirname, '..', 'product-detail.html'));
 });
 
 app.get('/product-detail.html', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'html', 'product-detail.html'));
+  res.sendFile(path.join(__dirname, '..', 'product-detail.html'));
+});
+
+app.get('/track-order', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'track-order.html'));
+});
+
+app.get('/track-order.html', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'track-order.html'));
 });
 
 // API Routes (if you add APIs later)
