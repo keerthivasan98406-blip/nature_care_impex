98ae9d94eceeb63ad95ee15d42c35f52c0bb5c10
keerthivasan98406-blip <keerthivasan98406@gmail.com>
2026-01-16 17:38:05 +0530

    Fix test script to load .env from correct path

--- server/test-database.js
+++ server/test-database.js
@@ -1,5 +1,6 @@
 /* Database Connection Test Script */
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
 const mongoose = require('mongoose');
 const Order = require('./models/Order');
 
